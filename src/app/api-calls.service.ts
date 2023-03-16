b5bf8465ff6f87a8be74320b7ab700d0fa03f8ad
Charles Path <charles.path@outlook.com>
2023-03-16 17:31:48 -0300

    Have added all the calls to my data http service, next, implement one of the modules to use it

--- src/app/api-calls.service.ts
+++ src/app/api-calls.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiCallsService {
+export class DataService {
 
-  constructor() { }
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  // Create
+  createData(data: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl + 'data', data);
+  }
+
+  // Read
+  getData(id: number): Observable<any> {
+    return this.http.get<any>(this.baseUrl + 'data/' + id);
+  }
+
+  getAllData(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl + 'data');
+  }
+
+  // Update
+  updateData(id: number, data: any): Observable<any> {
+    return this.http.put<any>(this.baseUrl + 'data/' + id, data);
+  }
+
+  // Delete
+  deleteData(id: number): Observable<any> {
+    return this.http.delete<any>(this.baseUrl + 'data/' + id);
+  }
 }
